82250b22a8a593222d890dee8584dab22f657e5c
skp29 <skp29@duke.edu>
2018-12-03 14:33:35 -0500

    adding a plot command to the code

--- src/plotData.R
+++ src/plotData.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 # Plot the points out:
-ggplot(gmpts, aes(longitude, latitude, group = status))+
+g <- ggplot(gmpts, aes(longitude, latitude, group = status))+
   scale_fill_gradient(low = "grey70", high = "grey30", guide = "none") +
   xlab("Longitude") +
   ylab("Latitude") +
@@ -10,3 +10,4 @@ ggplot(gmpts, aes(longitude, latitude, group = status))+
   stat_density_2d(aes(fill = ..level..), geom = "polygon", color = "black", size = 0.2, alpha = 0.5) +
   geom_point(color = "navy", size = .5, alpha = .4)
 
+plot(g)
