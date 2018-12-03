da91fc2894c656b5344643d423c8fdb9017480ca
skp29 <skp29@duke.edu>
2018-12-03 14:08:40 -0500

    adding all the code to calculate regression

--- src/readData.R
+++ src/readData.R
@@ -1,3 +1,17 @@
-typing something new
+library(readr)
+library(dplyr)
 
-another new thing added
+# Read in data
+# positional data about the RV Kahuna
+# kahuna <- read_csv('../../../presentations/2018/2018-12-03_DUML-RepResearch/data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv') 
+kahuna <- read_csv('data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE.csv')
+kStart <- kahuna %>% 
+  filter(status == 'start')
+
+# Read in Gm182 Data: 100 estimated positions of Gm182, augmented with focal follow data
+gm182UP <- read_csv('data/2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv') %>% 
+  mutate(status = 'userPoints')
+
+# Read in Gm182 Data: 100 estimated positions of Gm182
+gm182 <- read_csv('data/2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv') %>% 
+  mutate(status = 'noUserPoints')
